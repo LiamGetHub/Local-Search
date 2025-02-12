00ab2216a9319607eaecea681609057fe4bd728e
LiamGetHub <lgfitting@gmail.com>
2025-02-12 12:14:00 -0800

    "done"

--- localsearchtest.py
+++ localsearchtest.py
@@ -16,22 +16,30 @@ if __name__=="__main__":
     # 5x9 grid with 4 fixed spots and max_solutions with minimum cost
     #
 
-    search = LocalSearchPlayground(height=5, width=9, spots=[(0,2), (1,8), (3,0), (4, 3)], solutions=total_solutions)
+    # original search
+    #search = LocalSearchPlayground(height=5, width=9, spots=[(0,2), (1,8), (3,0), (4, 3)], solutions=total_solutions)
 
-    #search.show("Local search \n")
+    #search for problem #1 firefighter placement
+    #search = LocalSearchPlayground(height=10, width=10, spots=[(2,2), (6,1), (3,8), (9, 4), (5,2)], solutions=total_solutions)
 
-    #print("Testing the helper functions...")
-
-    #search.test_helper_functions(verbose = True)
-
+    #search for number 2, cell tower placement
+    search = LocalSearchPlayground(height=5, width=9, spots=[(2,0), (1,8), (3,7), (4, 5)], solutions=total_solutions)
 
 
-    #hill climbing
-    #print("\nHill climbing with random restart")
+    search.show("Local search \n")
 
-    #solution = search.hill_climbing(verbose=show_trace)
+    print("Testing the helper functions...")
 
-    #search.show("Final state", solution)
+    search.test_helper_functions(verbose = True)
+
+
+
+    #hill climbing w random restart
+    print("\nHill climbing with random restart")
+
+    solution = search.hill_climbing(verbose=show_trace)
+
+    search.show("Final state", solution)
 
 
 
